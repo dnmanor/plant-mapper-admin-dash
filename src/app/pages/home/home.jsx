05b35ece37e87d071c2298ad2d0fa9d28e3ed6ec
David Manor <david@dolphextechnologies.com>
2021-04-23 15:00:13 +0100

    [add] home page

---
+++ src/app/pages/home/home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import MenuBar from '../../components/menubar/menubar'
+import SideBar from '../../components/sidebar/sidebar'
+
+const Home = () => {
+    return (
+        <div>
+            <MenuBar />
+            <SideBar />
+        </div>
+    )
+}
+
+export default Home
